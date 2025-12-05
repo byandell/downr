a9849ba3195bb9c3e4c491a355adffc30794d97c
byandell <byandell@wisc.edu>
2025-12-05 10:49:11 -0600

    0.5.1 plot_null remove void to see area

--- R/plot_null.R
+++ R/plot_null.R
@@ -4,12 +4,11 @@
 #' Useful for shiny apps with plot output.
 #' @param msg character message
 #' 
-#' @importFrom ggplot2 ggplot aes geom_text theme_void
+#' @importFrom ggplot2 ggplot aes geom_text
 #' @importFrom rlang .data
 #' @export
 plot_null <- function(msg = "no data") {
   ggplot2::ggplot(data.frame(x = 1, y = 1), 
                   ggplot2::aes(.data$x, .data$y, label = msg)) +
-    ggplot2::geom_text(size = 10) + 
-    ggplot2::theme_void()
+    ggplot2::geom_text(size = 10)
 }
